bccfd2288620f28fbd4e6e37a2c722c2d271ecf5
faithlierheimer <flierheimer@gmail.com>
2021-02-08 18:46:53 -0700

    I FIXED THE IMPORT ISSUE!

---
+++ workouts_setup.sql
@@ -0,0 +1,9 @@
+create table workouts_jan_feb(
+workout_type varchar(100),
+full_date date,
+avg_hr bigint,
+max_hr bigint,
+active_cal bigint,
+total_cal bigint);
+
+select * from workouts_jan_feb;
